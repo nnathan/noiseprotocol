7fc21e208b9be0e07dc35c59baf2c0f4bb6edfc5
Naveen Nathan <naveen@naveennathans-iMac.local>
2016-05-06 11:46:26 +1000

    stub AES/ChaCha primitives

---
+++ noiseprotocol/crypto/aead.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+def AESGCM_encrypt(k, n, ad, plaintext):
+    raise Exception("Not implemented yet.")
+
+def AESGCM_decrypt(k, n, ad, ciphertext):
+    raise Exception("Not implemented yet.")
+
+def ChaChaPoly_encrypt(k, n, ad, plaintext):
+    raise Exception("Not implemented yet.")
+
+def ChaChaPoly_decrypt(k, n, ad, ciphertext):
+    raise Exception("Not implemented yet.")
+
