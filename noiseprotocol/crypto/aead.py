ce4164e5a9de98a2bfd8d25c625e3da0498bdfee
Naveen Nathan <nnathan@gmail.com>
2016-05-08 23:56:56 +1000

    redid aes_gcm primitives to use class/OO structure

--- noiseprotocol/crypto/aead.py
+++ noiseprotocol/crypto/aead.py
@@ -3,49 +3,51 @@
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.ciphers import algorithms, Cipher, modes
 
-def aes_gcm_encrypt(k, n, ad, plaintext):
+class AEADCipher(object):
+    def __init__(self, key):
+        if len(key) != 32:
+            raise ValueError('Invalid key size (32) for AES.')
+
+        self.key = key
+
+
+class AESGCM(AEADCipher):
     '''
        AES256-GCM from NIST SP 800-38D with 128-bit tags. The 96-bit nonce is formed by encoding
        32 bits of zeros followed by big-endian encoding of n.
-
-       Returns concatenation of authentication tag and ciphertext.
     '''
 
-    if len(k) != 32:
-        raise ValueError('Invalid key size (32) for AES.')
+    def encrypt(self, n, ad, plaintext):
+        '''
+           Encrypts and returns concatenation of authentication tag and ciphertext.
+        '''
+        encryptor = Cipher(
+            algorithms.AES(self.key),
+            modes.GCM(n, min_tag_length=16),
+            backend=default_backend()
+        ).encryptor()
 
-    encryptor = Cipher(
-        algorithms.AES(k),
-        modes.GCM(n, min_tag_length=16),
-        backend=default_backend()
-    ).encryptor()
+        encryptor.authenticate_additional_data(ad)
+        ciphertext = encryptor.update(plaintext) + encryptor.finalize()
 
-    encryptor.authenticate_additional_data(ad)
-    ciphertext = encryptor.update(plaintext) + encryptor.finalize()
+        return ciphertext + encryptor.tag
 
-    return ciphertext + encryptor.tag
 
-def aes_gcm_decrypt(k, n, ad, ciphertext):
-    '''
-       AES256-GCM from NIST SP 800-38D with 128-bit tags. The 96-bit nonce is formed by encoding
-       32 bits of zeros followed by big-endian encoding of n.
+    def decrypt(self, n, ad, ciphertext):
+        '''
+           Returns plaintext or raises InvalidTag exception if fail to authenticate.
+        '''
 
-       Returns plaintext or raises InvalidTag exception if fail to authenticate.
-    '''
+        if len(ciphertext) < 16:
+            raise ValueError('Invalid ciphertext size ({0}), must be >16 bytes.'.format(len(ciphertext)))
 
-    if len(k) != 32:
-        raise ValueError('Invalid key size (32) for AES.')
+        decryptor = Cipher(
+            algorithms.AES(self.key),
+            modes.GCM(n, ciphertext[-16:]),
+            backend=default_backend()
+        ).decryptor()
 
-    if len(ciphertext) < 16:
-        raise ValueError('Invalid ciphertext size ({0}), must be >16 bytes.'.format(len(ciphertext)))
+        decryptor.authenticate_additional_data(ad)
+        plaintext = decryptor.update(ciphertext[:-16]) + decryptor.finalize()
 
-    decryptor = Cipher(
-        algorithms.AES(k),
-        modes.GCM(n, ciphertext[-16:]),
-        backend=default_backend()
-    ).decryptor()
-
-    decryptor.authenticate_additional_data(ad)
-    plaintext = decryptor.update(ciphertext[:-16]) + decryptor.finalize()
-
-    return plaintext
+        return plaintext
--- tests/crypto/test_aead.py
+++ tests/crypto/test_aead.py
@@ -2,14 +2,12 @@
 
 import pytest
 
-from noiseprotocol.crypto.aead import aes_gcm_encrypt, aes_gcm_decrypt
+from noiseprotocol.crypto.aead import AESGCM
 
 @pytest.mark.parametrize('invalid_length', range(32) + [33])
-def test_aes_gcm_encrypt_invalid_key_length(invalid_length):
+def test_AESGCM_invalid_key_length(invalid_length):
     with pytest.raises(ValueError):
-        aes_gcm_encrypt('\x00' * invalid_length, 'x', 'x', 'x'*32)
+        AESGCM('\x00' * invalid_length)
 
-@pytest.mark.parametrize('invalid_length', range(32) + [33])
-def test_aes_gcm_decrypt_invalid_key_length(invalid_length):
-    with pytest.raises(ValueError):
-        aes_gcm_decrypt('\x00' * invalid_length, 'x', 'x', 'x'*32)
+def test_AESGCM_valid_key_length():
+    AESGCM('\x00'*32)
