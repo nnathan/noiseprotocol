64306087a1ac82f13a223c945ff7e1d291ecadc9
Naveen Nathan <nnathan@gmail.com>
2016-08-22 01:11:54 +1000

    crypto: use eccsnacks for curves

--- noiseprotocol/crypto/dh.py
+++ noiseprotocol/crypto/dh.py
@@ -3,111 +3,44 @@
 from collections import namedtuple
 import os
 
+from eccsnacks.curve25519 import scalarmult as c25519_scalarmult
+from eccsnacks.curve25519 import scalarmult_base as c25519_scalarmult_base
+from eccsnacks.curve448 import scalarmult as c448_scalarmult
+from eccsnacks.curve448 import scalarmult_base as c448_scalarmult_base
+
+
 DHKeyPair = namedtuple('DHKeyPair', ['private', 'public'])
 
-class DH(object):
-    pass
 
-class Curve25519(DH):
+class Curve25519(object):
 
-    _P = 2 ** 255 - 19
-    _A = 486662
+    name = '25519'
+    dhlen = 32
 
     def __init__(self):
         pass
 
-    @property
-    def name(self):
-        return '25519'
-
-    @staticmethod
-    def _expmod(b, e, m):
-        if e == 0:
-            return 1
-
-	t = Curve25519._expmod(b, e / 2, m) ** 2 % m
-
-        if e & 1: t = (t * b) % m
-        return t
-
-    @staticmethod
-    def _inv(x):
-        return Curve25519._expmod(x, Curve25519._P - 2, Curve25519._P)
-
-    # Addition and doubling formulas taken from Appendix D of "Curve25519:
-    # new Diffie-Hellman speed records".
-
-    @staticmethod
-    def _add((xn,zn), (xm,zm), (xd,zd)):
-        x = 4 * (xm * xn - zm * zn) ** 2 * zd
-        z = 4 * (xm * zn - zm * xn) ** 2 * xd
-        return (x % Curve25519._P, z % Curve25519._P)
-
-    @staticmethod
-    def _double((xn,zn)):
-        x = (xn ** 2 - zn ** 2) ** 2
-        z = 4 * xn * zn * (xn ** 2 + Curve25519._A * xn * zn + zn ** 2)
-        return (x % Curve25519._P, z % Curve25519._P)
-
-    @staticmethod
-    def _curve25519(n, base):
-        one = (base,1)
-        two = Curve25519._double(one)
-        # f(m) evaluates to a tuple containing the mth multiple and the
-        # (m+1)th multiple of base.
-        def f(m):
-            if m == 1: return (one, two)
-            (pm, pm1) = f(m / 2)
-            if (m & 1):
-                return (Curve25519._add(pm, pm1, one), Curve25519._double(pm1))
-            return (Curve25519._double(pm), Curve25519._add(pm, pm1, one))
-        ((x,z), _) = f(n)
-        return (x * Curve25519._inv(z)) % Curve25519._P
-
-    @staticmethod
-    def _unpack(s):
-        if len(s) != 32: raise ValueError('Invalid Curve25519 argument')
-        return sum(ord(s[i]) << (8 * i) for i in range(32))
-
-    @staticmethod
-    def _pack(n):
-        return ''.join([chr((n >> (8 * i)) & 255) for i in range(32)])
-
-    @staticmethod
-    def _clamp(n):
-        n &= ~7
-        n &= ~(128 << 8 * 31)
-        n |= 64 << 8 * 31
-        return n
-
-    @staticmethod
-    def _smult_curve25519(n, p):
-        n = Curve25519._clamp(Curve25519._unpack(n))
-        p = Curve25519._unpack(p)
-        return Curve25519._pack(Curve25519._curve25519(n, p))
-
-    @staticmethod
-    def _smult_curve25519_base(n):
-        n = Curve25519._clamp(Curve25519._unpack(n))
-        return Curve25519._pack(Curve25519._curve25519(n, 9))
-
     def generate_keypair(self):
         priv = os.urandom(32)
-        pub = self._smult_curve25519_base(priv)
+        pub = c25519_scalarmult_base(priv)
         return DHKeyPair(priv, pub)
 
     def dh(self, keypair, public_key):
-        return self._smult_curve25519(keypair.private, public_key)
+        return c25519_scalarmult(keypair.private, public_key)
 
 
-class Curve448(DH):
+class Curve448(object):
 
-    _P = 2 ** 448 - 2 ** 224 - 1
-    _A = 156326
+    name = '448'
+    dhlen = 56
 
     def __init__(self):
-        raise RuntimeError('Not Implemented Yet')
+        pass
 
-    @property
-    def name(self):
-        return '448'
+    def generate_keypair(self):
+        priv = os.urandom(56)
+        pub = c448_scalarmult_base(priv)
+        return DHKeyPair(priv, pub)
+
+    def dh(self, keypair, public_key):
+        return c448_scalarmult(keypair.private, public_key)
--- tests/crypto/test_dh.py
+++ tests/crypto/test_dh.py
@@ -4,7 +4,7 @@ from binascii import unhexlify, hexlify
 
 import pytest
 
-from noiseprotocol.crypto.dh import DHKeyPair, Curve25519
+from noiseprotocol.crypto.dh import DHKeyPair, Curve25519, Curve448
 
 
 def test_Curve25519_name():
@@ -22,3 +22,33 @@ def test_Curve25519_kat():
     c = Curve25519()
     k = c.dh(alice_keypair, bob_public_key)
     assert hexlify(k) == '4a5d9d5ba4ce2de1728e3bf480350f25e07e21c947d19e3376f09b3c1e161742'
+
+
+def test_Curve448_name():
+    assert Curve448().name == '448'
+
+
+# from https://www.ietf.org/rfc/rfc7748.txt
+def test_Curve448_kat():
+    alice_keypair = DHKeyPair(
+        unhexlify(
+            '9a8f4925d1519f5775cf46b04b5800d4ee9ee8bae8bc5565d498c28d'
+            'd9c9baf574a9419744897391006382a6f127ab1d9ac2d8c0a598726b'
+        ),
+        unhexlify(
+            '1c306a7ac2a0e2e0990b294470cba339e6453772b075811d8fad0d1d'
+            '6927c120bb5ee8972b0d3e21374c9c921b09d1b0366f10b65173992d'
+        )
+    )
+
+    bob_public_key = unhexlify(
+            '3eb7a829b0cd20f5bcfc0b599b6feccf6da4627107bdb0d4f345b430'
+            '27d8b972fc3e34fb4232a13ca706dcb57aec3dae07bdc1c67bf33609'
+    )
+
+    c = Curve448()
+    k = c.dh(alice_keypair, bob_public_key)
+    assert hexlify(k) == (
+        '07fff4181ac6cc95ec1c16a94a0f74d12da232ce40a77552281d282b'
+        'b60c0b56fd2464c335543936521c24403085d59a449a5037514a879d'
+    )
