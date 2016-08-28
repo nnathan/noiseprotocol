86b9e0a79bbf1170305ea8c7cbe02a7cfbe00cfc
Naveen Nathan <naveen@lastninja.net>
2016-08-28 13:52:43 +1000

    tests: working version of tests

--- t.py
+++ t.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import json
-from binascii import unhexlify as unhex, hexlify as hex
+from binascii import unhexlify, hexlify as hex
 
 from noiseprotocol.crypto.aead import *
 from noiseprotocol.crypto.hash import *
@@ -9,6 +9,11 @@ from noiseprotocol.crypto.dh import *
 from noiseprotocol.pattern import *
 from noiseprotocol.noise import *
 
+def unhex(s):
+    if s == None:
+        return None
+    else:
+        return unhexlify(s)
 
 def mock_curve(name):
     if name == '25519':
@@ -28,16 +33,25 @@ def mock_curve(name):
 
 
 def doit(v):
+    print v['name']
+    if 'SSK' in v['name']:
+        return
     name = v['name']
     aead = globals()[v['cipher']]
     init_dh = mock_curve(v['dh'])
     resp_dh = mock_curve(v['dh'])
     hash = globals()[v['hash']]
     pattern = globals()['Noise_'+v['pattern']]
+    one_way = False
+    if v['pattern'] in [ 'N', 'X', 'K' ]:
+        one_way = True
+
     prefix = 'Noise'
+    if v.get('init_psk', None) is not None:
+        prefix += 'PSK'
     assert name == '_'.join([prefix, pattern.name, init_dh.name, aead.name, hash.name])
-    init_prologue = v.get('init_prologue')
-    resp_prologue = v.get('resp_prologue')
+    init_prologue = unhex(v.get('init_prologue'))
+    resp_prologue = unhex(v.get('resp_prologue'))
     # sanity check
     assert init_prologue == resp_prologue
 
@@ -46,23 +60,65 @@ def doit(v):
     resp_ephemeral = unhex(v.get('resp_ephemeral'))
     resp_dh.priv = resp_ephemeral
 
+    init_psk = unhex(v.get('init_psk', None))
+    resp_psk = unhex(v.get('resp_psk', None))
+
+    init_static = unhex(v.get('init_static', None))
+    resp_static = unhex(v.get('resp_static', None))
+    if init_static is not None:
+        dh = init_dh()
+        dh.priv = init_static
+        init_static = dh.generate_keypair()
+    if resp_static is not None:
+        dh = resp_dh()
+        dh.priv = resp_static
+        resp_static = dh.generate_keypair()
+    init_semiephemeral = unhex(v.get('init_semiephemeral', None))
+    resp_semiephemeral = unhex(v.get('resp_semiephemeral', None))
+    init_remote_static = unhex(v.get('init_remote_static', None))
+    resp_remote_static = unhex(v.get('resp_remote_static', None))
+    init_remote_semiephemeral = unhex(v.get('init_remote_semiephemeral', None))
+    resp_remote_semiephemeral = unhex(v.get('resp_remote_semiephemeral', None))
+
     init, resp = NoiseHandshake(aead, hash, init_dh), NoiseHandshake(aead, hash, resp_dh)
 
-    init.Initialize(pattern, True, init_prologue, None, None, None, None)
-    resp.Initialize(pattern, False, resp_prologue, None, None, None, None)
+    init.Initialize(pattern, True, init_prologue, init_static, init_semiephemeral, init_remote_static, init_remote_semiephemeral, init_psk)
+    resp.Initialize(pattern, False, resp_prologue, resp_static, resp_semiephemeral, resp_remote_static, resp_remote_semiephemeral, resp_psk)
 
-    for m in v['messages']:
-        print m
+    send, recv = init, resp
+
+    messages = v['messages']
+    while len(messages) > 0:
+        m = messages.pop(0)
         ct = unhex(m['ciphertext'])
-        payload = unhex(m.get('payload', ''))
-        out1, ic1, ic2 = init.WriteMessage(payload)
         print "ct:\t{0}".format(hex(ct))
-        print "out1:\t{0}".format(hex(out1))
-        assert out1 == ct
-        out2, rc1, rc2 = resp.ReadMessage(out1)
-        print "out2:\t{0}".format(hex(out2))
-        init, resp = resp, init
+        payload = unhex(m.get('payload', ''))
+        print "p:\t{0}".format(hex(payload))
+        iout, ic1, ic2 = send.WriteMessage(payload)
+        assert ct == iout
+        print "iout:\t{0}".format(hex(iout))
+        rout, rc1, rc2 = recv.ReadMessage(iout)
+        print "rout:\t{0}".format(hex(rout))
+        send, recv = recv, send
+        if ic1 is not None and ic2 is not None and rc1 is not None and rc2 is not None:
+            break
+
+    if resp.initiator and not one_way:
+        ic2, ic1, rc2, rc1 = rc1, rc2, ic1, ic2
+    for m in messages: 
+        ct = unhex(m['ciphertext'])
+        print "ct:\t{0}".format(hex(ct))
+        payload = unhex(m.get('payload', ''))
+        print "p:\t{0}".format(hex(payload))
+        iout = ic1.EncryptWithAd('', payload)
+        print "iout:\t{0}".format(hex(iout))
+        rout = rc1.DecryptWithAd('', iout)
+        assert ct == iout
+        if not one_way:
+            ic2, ic1, rc2, rc1 = rc1, rc2, ic1, ic2
 
 j = json.load(open('cacaphony.txt'))
+#j = json.load(open('z'))
 
-doit(j['vectors'][0])
+for v in j['vectors']:
+    doit(v)
